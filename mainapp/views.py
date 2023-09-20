874eb94abbd4ca4e301f9a25b81b1925958ed2d7
Than Than Soe <thanthansoe15151@gmail.com>
2023-09-20 08:07:31 +0630

    delete-wed

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,66 +5,66 @@ from . models import *
 def dashboard(request):
      districts = District.objects.all()
      townships = Township.objects.all()
+     
      context = {
           'districts':districts,
           'townships':townships,
+          
      }
      return render(request, 'backend/layouts/dashboard.html', context)
-     
-          
+
+
+
 def district(request):
      districts = District.objects.all()
      context = {
           'districts':districts,
      }
+
      return render(request, 'backend/pages/district.html', context)
 
-
-
 def township(request):
+    
+
      townships = Township.objects.all()
      context = {
           'townships':townships,
+        
      }
      return render(request, 'backend/pages/township_detail.html', context)
 
 
-        
+
+
+
+
 def township_detail(request, pk=id):
      districts = District.objects.all()
      townships = Township.objects.all()
+     
      township_townships = Township.objects.filter(district_id=pk)
+     
      context = {
           'township_townships':township_townships,
           
           'districts':districts,
           'townships':townships,
+          
      }
      return render(request, 'backend/pages/township_detail.html', context)
-          
-     
-     
 
 
 def district_detail(request, pk=id):
      districts = District.objects.all()
      townships = Township.objects.all()
+    
      district_townships = Township.objects.filter(district_id=pk)
+     
      context = {
           'districts':districts,
           'townships':townships,
           
           'district_townships':district_townships,
+          
      }
      return render(request, 'backend/pages/district_detail.html', context)
-    
-
-
-
-
-
-
-
-    
-     
-          
